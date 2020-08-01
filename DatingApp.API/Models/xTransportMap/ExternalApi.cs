25a21719f419f9fc326b9945aa19565c3a54062f
Josh Chumber <jchumber@uxli.co.uk>
2020-08-01 19:40:00 +0100

    Generic login page

--- DatingApp.API/Models/xTransportMap/ExternalApi.cs
+++
@@ -1,9 +0,0 @@
-namespace DatingApp.API.Models.xTransportMap
-{
-    public class ExternalApi
-    {
-        public int id { get; set; }
-        public string company_code { get; set; }
-        public string company_name { get; set; }
-    }
-}
